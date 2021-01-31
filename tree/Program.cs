dffa188ea5007cff875e3970480bb5941670c490
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-01-31 12:31:02 +0300

    Update

--- tree/Program.cs
+++ tree/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace tree
-{  
+{
     class Program
     {
         List<Member> myMembers = new List<Member>();
@@ -21,12 +21,14 @@ namespace tree
             var Lia = new Member("Foxy", "2", new DateTime(1, 2, 3));
             var Foxy = new Member("Bob", "2", new DateTime(1, 2, 3));
             var Foxy2 = new Member("Sonya", "2", new DateTime(1, 2, 3));
-            var Charly = new Member(Foxy,Lia, "Charly", "2", new DateTime(1, 2, 3));
-            var famaly = Larry + Marry;
+            var Charly = new Member(Foxy, Lia, "Charly", "2", new DateTime(1, 2, 3));
             var famaly2 = Marry3 + Marry4;
-            myMembers.AddRange(new List<Member> { mamber, Larry, Marry2,Lia });
-            mamber.AddChildren(Charly);
             
+            myMembers.AddRange(new List<Member> { mamber, Larry, Marry2, Lia, Marry,
+                Marry3, Marry4, Larry2, Foxy, Foxy2, Charly });
+
+            mamber.AddChildren(Charly);
+
             mamber.AddChildren(Larry);
             mamber.AddChildren(Larry2);
 
@@ -45,8 +47,8 @@ namespace tree
 
             ShowTree(mamber, 0);
         }
-        void ShowTree(Member myMember=null, int Level=0)
-    {
+        void ShowTree(Member myMember = null, int Level = 0)
+        {
             if (myMember == null)
             {
                 for (int i = 0; i < myMembers.Count; i++)
@@ -62,71 +64,112 @@ namespace tree
             //myMember = myMember ?? myMembers.FirstOrDefault(m => m.ParentDad == null && m.ParentMom == null);
             if (myMember == null)
                 return;
-        Level++;
+            Level++;
 
-        Console.WriteLine($"{myMember.LastName}");
+            Console.WriteLine($"{myMember.LastName}");
 
-        foreach (var item in myMember.Clildrens)
-        {
-            for (int i = 0; i < Level; i++)
+            foreach (var item in myMember.Clildrens)
             {
-                Console.Write("   ");
+                for (int i = 0; i < Level; i++)
+                {
+                    Console.Write("   ");
+                }
+
+                Console.Write("|---");
+                ShowTree(item, Level);
             }
 
-            Console.Write("|---");
-            ShowTree(item, Level);
         }
-
-    }
         static void Main(string[] args)
         {
-            Program pro = new Program();
-            pro.Run();
-            Console.WriteLine("Enter 1 more relative- press eny 1");
-            string chose = Console.ReadLine();
             bool isContinue = true;
-            while (isContinue)
+            while (true)
             {
+                Program pro = new Program();
+                pro.Run();
+
+                Console.WriteLine("Enter 1 more relative- press eny 1");
+                Console.WriteLine("Exit the program -press 2");
+                string chose = Console.ReadLine();
+
+
                 switch (chose)
                 {
                     case "1":
                         {
-                            Console.WriteLine("Enter lastName");
-                            string lName = Console.ReadLine();
-                            Console.WriteLine("Enter firstName");
-                            string fName = Console.ReadLine();
-                            var x = new Member(lName, fName);
-                            Console.WriteLine("Enter the first name of parent");
-                            string parentName = Console.ReadLine();
-
-                            //var foundParent = pro.myMembers.FirstOrDefault(m => m.LastName == parentName);
-
-                            Member foundParent = null;
-                            foreach (var memb in pro.myMembers)
+                            while (isContinue)
                             {
-                                if (memb.LastName == parentName)
+                                Console.WriteLine("Enter lastName");
+                                string lName = Console.ReadLine();
+                                Console.WriteLine("Enter firstName");
+                                string fName = Console.ReadLine();
+                                var x = new Member(lName, fName);
+                                Console.WriteLine("Enter the first name of parent");
+                                string parentName = Console.ReadLine();
+
+                                //var foundParent = pro.myMembers.FirstOrDefault(m => m.LastName == parentName);
+
+                                Member foundParent = null;
+                                foreach (var memb in pro.myMembers)
                                 {
-                                    foundParent = memb;
-                                    break;
+                                    if (memb.LastName == parentName)
+                                    {
+                                        foundParent = memb;
+                                        break;
+                                    }
+                                }
+
+                                if (foundParent != null)
+                                {
+                                    foundParent.AddChildren(x);
+                                    pro.myMembers.Add(x);
+                                    Console.Clear();
+                                    pro.ShowTree();
+                                }
+                                else 
+                                {
+                                    Console.Clear();
+                                    pro.ShowTree();
+                                }
+
+                                Console.WriteLine("continue yes? no- press eny key?");
+                                if (Console.ReadLine() != "yes")
+                                {
+                                    Console.Clear();
+                                    isContinue = false;
+                                  
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    pro.ShowTree();
                                 }
                             }
+                            break;
+                        }
 
-                            if (foundParent != null)
+                    case "2":
+                        {
+                            Console.WriteLine("Exit programm - y; No exit programm - n");
+                            var result = Console.ReadLine();
+                            if (result == "y")
                             {
-                                foundParent.AddChildren(x);
-                                pro.myMembers.Add(x);
-                                pro.ShowTree();
+                                //Environment.Exit(0);
+                                isContinue = false;
                             }
-                            
-                            Console.WriteLine("continue yes? no- press eny key?");
-                            if(Console.ReadLine() != "yes")
-                            isContinue = false;
-                            
+                            else
+                            {
+                                Console.Clear();
+                                break;
+                            }
 
                             break;
                         }
                     default:
-                        break;
+                        {
+                            Console.Clear();
+                            break;
+                        }
                 }
             }
         }
